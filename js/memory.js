582e31b818efc63857a19ac29e594fbf374f3d9c
Dan Otter <danco77@virginmedia.com>
2021-07-20 12:43:39 +0000

    fixed issue where clicking anywhere on overlay started/restared the game. Now only clicking the text will start/restart a game

--- js/memory.js
+++ js/memory.js
@@ -1,50 +1,47 @@
 const card = document.querySelectorAll('.card');
-let cards = [...card];
 
 
-// Clicker Counter // 
+// Clicker Counter
 
 let moves = document.querySelectorAll('.card');
-let counter = document.querySelector('#counter')
+let counter = document.querySelector('#counter');
 
-for (var i = 0 ; i < moves.length; i++) {
-   moves[i].addEventListener('click' , countUp) ; 
+for (var i = 0; i < moves.length; i++) {
+    moves[i].addEventListener('click', countUp);
 }
 
-function countUp(){
+function countUp() {
     counter.innerHTML++;
 }
 
 
-// timer / game over //
-var timeLeft = document.getElementsByClassName('overlay-text')
-    for (var i = 0; i < timeLeft.length; i++) {
-    timeLeft[i].addEventListener('click', function(){
-    var timeLeft = 60;
-    var gameTimer = setInterval(function(){
-    timeLeft--;
-    document.getElementById('time-remaining').textContent = timeLeft;
-    if(timeLeft <= 0)
-        clearInterval(gameTimer);
-    },1000);
-});
-}
-
-
-`document.getElementById('game-start').addEventListener("click", function(){
-    
-});`
-
-var reset = document.getElementsByClassName('overlay-text')
-    for (var i = 0; i < reset.length; i++) {
-    reset[i].addEventListener('click', function(){
-        setTimeout(endScreen, 60000);
+// timer
+var timeLeft = document.getElementsByClassName('overlay-text');
+for (var i = 0; i < timeLeft.length; i++) {
+    timeLeft[i].addEventListener('click', function () {
+        var timeLeft = 5;
+        var gameTimer = setInterval(function () {
+            timeLeft--;
+            document.getElementById('time-remaining').textContent = timeLeft;
+            if (timeLeft <= 0)
+                clearInterval(gameTimer);
+        }, 1000);
     });
 }
 
-function endScreen(){
-    victory();
-    
+var reset = document.getElementsByClassName('overlay-text');
+for (var i = 0; i < reset.length; i++) {
+    reset[i].addEventListener('click', function () {
+        setTimeout(endScreen, 5000);
+    });
+}
+
+
+// conditions
+
+function endScreen() {
+    gameOver();
+
 }
 
 function gameOver() {
@@ -56,66 +53,76 @@ function victory() {
 }
 
 
-// game start //
+`document.getElementById('game-start').addEventListener("click", function(){
+    
+});`;
+
+
+// game start
 const deck = document.querySelector(".playarea");
 
-function startGame(){
+function startGame() {
     let resetFlip = document.querySelectorAll(".card");
     for (let i = 0; i < resetFlip.length; i++) {
         resetFlip[i].style.transform = "rotateY(0deg)";
     }
-    setTimeout(function(){
+    setTimeout(function () {
         shuffle();
         moves = 0;
         counter.innerHTML = moves;
-        document.getElementById("time-remaining").textContent = 60;
-        
-    
-    },750);
+        document.getElementById("time-remaining").textContent = 5;
+
+
+    }, 750);
 }
 
-
 function ready() {
-    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-
+   // let overlays = Array.from(document.getElementsByClassName('overlay'));
+    let overlayTexts = Array.from(document.getElementsByClassName('overlay-text'));
+`
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
-        setTimeout(function(){
-            overlay.classList.remove('visible');
-        }, 800); 
+            setTimeout(function () {
+                overlay.classList.remove('visible');
+            }, 800);
+        });
+    });
+}`
+    overlayTexts.forEach(overlayText => {
+        overlayText.addEventListener('click', () => {
+            overlayText.parentElement.classList.remove('visible');
             startGame();
         });
     });
 }
 
 
-// Card randomiser // 
+// Card randomiser
 
 function shuffle() {
     card.forEach(card => {
         let randomPos = Math.floor(Math.random() * 12);
         card.style.order = randomPos;
     });
-};
+}
 
 
-// Card flip function //
+// Card flip function
 
-function flip(event){
-	var element = event.currentTarget;
-	if (element.className === "card") {
-        if(element.style.transform == "rotateY(180deg)") {
+function flip(event) {
+    var element = event.currentTarget;
+    if (element.className === "card") {
+        if (element.style.transform == "rotateY(180deg)") {
             element.style.transform = "rotateY(0deg)";
-        }
-        else {
-        element.style.transform = "rotateY(180deg)";
+        } else {
+            element.style.transform = "rotateY(180deg)";
         }
     }
-};
+}
 
 
 
-if(document.readyState === 'loading') {
+if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', ready());
 } else {
     ready();
